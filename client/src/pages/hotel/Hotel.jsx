04ec20b5fe8a2e8af651b47169c6e41ab9a605b2
ThienBNguyen <thien.nguyen30191@gmail.com>
2022-05-31 12:59:09 -0500

    complete ui

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -1,12 +1,16 @@
 import './hotel.css';
 import NavBar from '../../components/navbar/NavBar';
 import Header from '../../components/header/Header';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot, faCircleXmark, faCircleArrowLeft, faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import MailList from '../../components/mailList/MailList';
 import Footer from '../../components/footer/Footer';
+import { useState } from 'react';
 
 const Hotel = () => {
+    const [slideNumber, setSlideNumber] = useState(0)
+    const [open, setOpen] = useState(false)
+    const [openModal, setOpenModal] = useState(false)
     const images = [
         "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
         "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
@@ -14,11 +18,50 @@ const Hotel = () => {
         "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
         "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
     ];
+    const handleOpen = (i) => {
+        setSlideNumber(i);
+        setOpen(true);
+    }
+    const handleMove = (direction) => {
+        let newSlideNumber;
+        if (direction == "l") {
+            newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+        } else {
+            newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+        }
+        setSlideNumber(newSlideNumber)
+    }
     return (
         <div>
             <NavBar />
             <Header type="list" />
             <div className="hotelContainer">
+                {open && (
+                    <div className="slider">
+                        <FontAwesomeIcon
+                            icon={faCircleXmark}
+                            className="close"
+                            onClick={() => setOpen(false)}
+                        />
+                        <FontAwesomeIcon
+                            icon={faCircleArrowLeft}
+                            className="arrow"
+                            onClick={() => handleMove("l")}
+                        />
+                        <div className="sliderWrapper">
+                            <img
+                                src={images[slideNumber]}
+                                alt=""
+                                className="sliderImg"
+                            />
+                        </div>
+                        <FontAwesomeIcon
+                            icon={faCircleArrowRight}
+                            className="arrow"
+                            onClick={() => handleMove("r")}
+                        />
+                    </div>
+                )}
                 <div className="hotelWrapper">
                     <button className="bookNow">
                         reser or book now</button>
@@ -33,10 +76,10 @@ const Hotel = () => {
                     <span className="hotelPriceHighlight">
                         book a stay ove 114 at this property and get a free airport taxi</span>
                     <div className="hotelImages">
-                        {images.map(photo => (
-                            <div className="hotelImgWrapper">
+                        {images.map((photo, i) => (
+                            <div className="hotelImgWrapper" key={i}>
 
-                                <img src={photo} alt="" className="hotelImg" />
+                                <img src={photo} alt="" className="hotelImg" onClick={() => handleOpen(i)} />
                             </div>
                         ))}
                     </div>
